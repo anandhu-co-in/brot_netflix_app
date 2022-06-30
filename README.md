65f114c8e4a1a447d7f36edd96d69a76f24568c1
anandhukrishnan.v <mail.anandhu@gmail.com>
2022-06-30 08:26:57 +0530

    Search screen idle's row items

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ design :
 downloads page
 
 ![img.png](img.png)
+
+search page - search idle
+
+![img_1.png](img_1.png)
+
+search page - search results
+
+![img_2.png](img_2.png)
--- lib/presentation/search/widget/search_idle.dart
+++ lib/presentation/search/widget/search_idle.dart
@@ -1,3 +1,5 @@
+import 'package:brot_netflix_app/core/colours.dart';
+import 'package:brot_netflix_app/core/constants.dart';
 import 'package:flutter/material.dart';
 
 class SearchIdleWidget extends StatelessWidget {
@@ -8,17 +10,19 @@ class SearchIdleWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        kHeight,
         const Text(
-          "Top Search",
+          "Top Searches",
           style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
+        kHeight,
         Expanded(
           child: ListView.separated(
               itemBuilder: (ctx, index) {
-                return Text('Listviw item');
+                return TopSearchTileItem();
               },
               separatorBuilder: (ctx, index) {
-                return Text('Separator');
+                return kHeight;
               },
               itemCount: 210),
         )
@@ -27,7 +31,47 @@ class SearchIdleWidget extends StatelessWidget {
   }
 }
 
+class TopSearchTileItem extends StatelessWidget {
+  const TopSearchTileItem({Key? key}) : super(key: key);
+
+  final imageUrl =
+      "https://www.mmppicture.co.in/wp-content/uploads/2020/07/The-Hero-Action-Movie-Poster.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Row(
+      children: [
+        Container(
+            // color: Colors.red,
+            width: screenWidth * .3,
+            height: screenWidth * .22,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover, image: NetworkImage(imageUrl)),
+            )),
+        kWidth,
+        const Expanded(
+          //Without this expand, when long movie names, there is overflow. Why? dont ask me!
+          child: Text(
+            'Movie Name',
+            style: TextStyle(
+                color: textWhite, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        kWidth,
+        const Icon(
+          Icons.play_circle_outline_outlined,
+          color: Colors.white,
+          size: 40,
+        )
+        // TODO - the original icon had thinner outlines, etc maybe create on our own using circle avarat?
+      ],
+    );
+  }
+}
+
 // NOTE
 // Fixed that listview height issues. But i need to further understand how it works
-// TODO
-// Populate the listview
+// TODO - Start designing the search results screen next
