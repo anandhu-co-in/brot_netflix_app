22454ecc759bfda2a74bd6a2e36469795955fd76
anandhukrishnan.v <mail.anandhu@gmail.com>
2022-08-19 00:06:03 +0530

    did top menu, used notificationlistener to show/hide on scroll notification, animation didnt work though

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -2,6 +2,9 @@ import 'package:brot_netflix_app/core/constants.dart';
 import 'package:brot_netflix_app/presentation/home/widget/background_card.dart';
 import 'package:brot_netflix_app/presentation/widgets/row_of_cards_with_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+ValueNotifier<bool> showTopMenu = ValueNotifier(true);
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({Key? key}) : super(key: key);
@@ -11,32 +14,95 @@ class ScreenHome extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: paddingPage,
-        child: ListView(
-          children: const [
-            BackgroundCard(),
-            kHeight,
-            RowOfCardsWithTitle(title: "Released in the Past Year"),
-            kHeight,
-            RowOfCardsWithTitle(
-              title: "Trending Now",
-            ),
-            kHeight,
-            RowOfNumberCardsWithTitle(
-              title: "Top 10 in India Today (Number Cards)",
-            ),
-            kHeight,
-            RowOfCardsWithTitle(
-              title: "Tense Dramas",
-            ),
-            kHeight,
-            RowOfCardsWithTitle(
-              title: "South Indian Cinema",
-            ),
-          ],
+        child: NotificationListener<UserScrollNotification>(
+          onNotification: (notification) {
+            if (notification.direction == ScrollDirection.forward) {
+              print("Show");
+              showTopMenu.value = true;
+            } else if (notification.direction == ScrollDirection.reverse) {
+              print("Hide");
+              showTopMenu.value = false;
+            }
+            return true; //return true cancel notification bubbling to parent
+          },
+          child: Stack(
+            children: [
+              ListView(
+                children: const [
+                  BackgroundCard(),
+                  kHeight,
+                  RowOfCardsWithTitle(title: "Released in the Past Year"),
+                  kHeight,
+                  RowOfCardsWithTitle(
+                    title: "Trending Now",
+                  ),
+                  kHeight,
+                  RowOfNumberCardsWithTitle(
+                    title: "Top 10 in India Today (Number Cards)",
+                  ),
+                  kHeight,
+                  RowOfCardsWithTitle(
+                    title: "Tense Dramas",
+                  ),
+                  kHeight,
+                  RowOfCardsWithTitle(
+                    title: "South Indian Cinema",
+                  ),
+                ],
+              ),
+              ValueListenableBuilder(
+                valueListenable: showTopMenu,
+                builder: (BuildContext ctx, bool value, _) {
+                  return value
+                      ? Container(
+                          color: Colors.black.withOpacity(.3),
+                          height: 90,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                children: [
+                                  Image.network(
+                                    "https://www.geekslop.com/wp-content/uploads/2020/06/netflix-logo-letter-n.png",
+                                    width: 50,
+                                  ),
+                                  const Spacer(),
+                                  const Icon(
+                                    Icons.cast,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                  kWidth,
+                                  Container(
+                                    color: Colors.blue,
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                  kWidth,
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: const [
+                                  Text('TV Shows'),
+                                  Text('Movies'),
+                                  Text('Categories')
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      : kHeight;
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// TODO - Implement scrolling for the top box - video timestamp 23:02
+// TODO - (If possible), fade-in/fade-out animation top menu container when scrolling
+// TODO - Do New&Hot page
