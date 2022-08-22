0575784873d534a86999e5315e0f7dc92e6cd942
anandhukrishnan.v <mail.anandhu@gmail.com>
2022-08-22 21:10:33 +0530

    blindly coding, addreed a downloads class, ran that build runner command and generated 2 more files. I completely fogot what these were

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ tmdb : credentials
 username : brototype
 password : 1234
 API KEY : 86d69a245a7d86220c2aeab2255e4095
+
+
+Build runner command :
+flutter packages pub run build_runner watch --use-polling-watcher --delete-conflicting-outputs
--- lib/domain/core/api_end_points.dart
+++ lib/domain/core/api_end_points.dart
@@ -4,3 +4,5 @@ import 'package:brot_netflix_app/infrastructure/api_key.dart';
 class ApiEndPoints {
   static const downloads = "$baseURL/trending/all/day?api_key=$APIKEY";
 }
+
+// So that i can access the api urls like ApiEndPoints.downloads etc..
