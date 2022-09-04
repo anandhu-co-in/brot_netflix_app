be078fa69357e918152e0b30bcb482de6d414551
anandhukrishnan.v <mail.anandhu@gmail.com>
2022-09-04 15:35:45 +0530

    blindcoding - injectableConfigure, and modifying main function and callin git there

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:brot_netflix_app/core/colours.dart';
+import 'package:brot_netflix_app/domain/core/di/injectable.dart';
 import 'package:brot_netflix_app/presentation/main_page/screen_main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureInjection();
   runApp(const MyApp());
 }
 
